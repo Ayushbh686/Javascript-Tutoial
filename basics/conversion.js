915e47a55cbed3a25ea3a36f72677065c6182991
Ayushbh686 <159467620+Ayushbh686@users.noreply.github.com>
2024-03-30 17:56:03 +0000

    operations and conversions done

---
+++ basics/conversion.js
@@ -0,0 +1,16 @@
+let score="ayush";
+console.log(typeof score);
+let num="ayush123";
+let value=Number(num);  //Nan
+console.log(value);
+console.log(typeof value);
+let num2="1234";
+console.log(typeof Number(num2));
+console.log(Number(num2)+2);   //1236
+console.log(num2+2);   //12342
+
+//anything other than empty and 0 when converted to boolean will be true 
+let someNumber = 45;
+console.log(someNumber);
+let str=String(someNumber);  // S capital
+console.log(str);
---
+++ basics/operations.js
@@ -0,0 +1,3 @@
+let value=3;
+let negval=-value;
+//console.log(negval);
